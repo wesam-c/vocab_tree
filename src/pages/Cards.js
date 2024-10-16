fd606ae12f0f4dabd7d9433fc05a722507d696ce
wesam-c <wesameldeeb15@gmail.com>
2024-10-16 14:52:36 +0400

    the card page

---
+++ src/pages/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AllCards from '../components/TheFlashcards/AllCards';
+
+
+const CardsPage = () => {
+  return (
+    
+      
+      <div className="bg-lime-200 min-h-screen dark:bg-gray-800">
+      <div className="container mx-auto pt-24 ">
+        
+      <AllCards />
+        
+      </div>
+    </div>
+  );
+};
+
+export default CardsPage;
